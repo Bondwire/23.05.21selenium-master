2bb7152929d8a0f71b40d528986aa8eeaa264607
Bondwire <68152742+Bondwire@users.noreply.github.com>
2021-05-23 13:40:56 +0200

    Initial commit

---
+++ src/test/java/przyklad1/SearchTest.java
@@ -0,0 +1,49 @@
+package przyklad1;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.junit.*;
+
+public class SearchTest {
+//Maven nie rozumie jeżeli klasa nie będzie .......cośTEst,
+    // klasa nie może mieć innej nazwy niż fisdufisduTest,
+    //invalidateCasches nie lubi spacji rebuild the project,
+    //może wywalić exception za 1 razem,
+    //
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        // Uruchom nowy egzemplarz przeglądarki Chrome
+        driver = new ChromeDriver();
+     //   driver = new FirefoxDriver + kolejne system property
+        //konstruktor klasy Chrome Driver
+        // Zmaksymalizuj okno przeglądarki
+        driver.manage().window().maximize();
+        // Przejdź do Google
+        driver.get("http://www.google.com");
+    }
+    @Test //adnotacja Frameworku JUnit
+    public void testGoogleSearch() {
+        // Znajdź element wprowadzania tekstu na podstawie jego nazwy
+        WebElement element = driver.findElement(By.name("q"));
+        // Wyczyść teskst zapisany w elemencie
+        element.clear();
+        //get wchodzi na stronę
+        // Wpisz informacje do wyszukania
+        element.sendKeys("Testowanie Selenium WebDriver"); //wpisuje literka po literce
+        //keys.enter, keys.alt wciskanie klawiszy
+
+        // Prześlij formularz
+        element.submit();
+    }
+    @After //adnotacja frameworku JUnit
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+       // driver.quit();
+    }
+}
